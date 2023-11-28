abc5d013f6c3bfa640895597805905ce2d21324e
Abhishek Yadav <41482390+Abhi181818@users.noreply.github.com>
2023-11-28 09:48:58 +0530

    Update RichTextEditor.jsx

--- client/src/Pages/AskQuestion/RichTextEditor.jsx
+++ client/src/Pages/AskQuestion/RichTextEditor.jsx
@@ -1,56 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import "./RichTextEditor.css"
 const RichTextEditor = () => {
-  const [content, setContent] = useState('');
-  const [isBold, setIsBold] = useState(false);
-  const [isItalic, setIsItalic] = useState(false);
-  const [isUnderline, setIsUnderline] = useState(false);
-
-  const handleBoldClick = () => {
-    setIsBold(!isBold);
-  };
-
-  const handleItalicClick = () => {
-    setIsItalic(!isItalic);
-  };
-
-  const handleUnderlineClick = () => {
-    setIsUnderline(!isUnderline);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
-  };
-
-  const getEditorStyles = () => {
-    let styles = {};
-    if (isBold) {
-      styles = { ...styles, fontWeight: 'bold' };
+  const contentRef = useRef(null);
+  const formatDoc = (cmd, value = null) => {
+    contentRef.current.focus()
+    if (value) {
+      document.execCommand(cmd, false, value);
+    } else {
+      document.execCommand(cmd);
     }
-    if (isItalic) {
-      styles = { ...styles, fontStyle: 'italic' };
-    }
-    if (isUnderline) {
-      styles = { ...styles, textDecoration: 'underline' };
-    }
-
-    return styles;
   };
+
 
   return (
-    <div className='editor'>
-      <h2>
-        <b>Rich Text Editor</b>
-      </h2>
-      <button className='editor-btn' onClick={handleBoldClick}>Bold</button>
-      <button className='editor-btn' onClick={handleItalicClick}>Italic</button>
-      <button onClick={handleUnderlineClick} className='editor-btn'>Underline</button>
-      <div
-        contentEditable
-        style={{ border: '1px solid  #ccc', padding: '8px', marginBottom: '10px', borderRadius: '12px', minHeight: '100px', ...getEditorStyles() }}
-        onInput={handleContentChange}
-      >
-        {content}
+    <div className="container">
+      <div className="toolbar">
+        <div className="toolbar_item" >
+          <button onClick={() => formatDoc('bold')}>
+            <i className="fas fa-bold"></i>
+          </button>
+          <button onClick={() => formatDoc('italic')}>
+            <i className="fas fa-italic"></i>
+          </button>
+          <button onClick={() => formatDoc('underline')}>
+            <i className="fas fa-underline"></i>
+          </button>
+          <button onClick={() => formatDoc('strikeThrough')}>
+            <i className="fas fa-strikethrough"></i>
+          </button>
+          <button onClick={() => formatDoc('justifyLeft')}>
+            <i className="fas fa-align-left"></i>
+          </button>
+          <button onClick={() => formatDoc('justifyCenter')}>
+            <i className="fas fa-align-center"></i>
+          </button>
+          <button onClick={() => formatDoc('justifyRight')}>
+            <i className="fas fa-align-right"></i>
+          </button>
+          <select onChange={(e) => formatDoc('fontSize', e.target.value)}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3" defaultValue="3">
+              3
+            </option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            Color
+            <input type="color" onChange={(e) => formatDoc('foreColor', e.target.value)} />
+          </div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            Hilight
+            <input type="color" onChange={(e) => formatDoc('backColor', e.target.value)} />
+          </div>
+
+          <button onClick={() => formatDoc('insertUnorderedList')}>
+            <i className="fas fa-list-ul"></i>
+          </button>
+          <button onClick={() => formatDoc('insertOrderedList')}>
+            <i className="fas fa-list-ol"></i>
+          </button>
+          <button onClick={() => formatDoc('indent')}>
+            <i className="fas fa-indent"></i>
+          </button>
+          <button onClick={() => formatDoc('outdent')}>
+            <i className="fas fa-outdent"></i>
+          </button>
+
+        </div>
+      </div>
+      <div id="content" ref={contentRef} style={{ padding: "5px", marginTop: "20px", marginBottom: "12px", height: "auto", minHeight: "120px", border: "1px solid black", borderRadius: "15px", overflowWrap: "break-word" }} placeholder='Your Text Here' contentEditable="true" spellCheck="false">
       </div>
     </div>
   );
