73e3baab1989ec65b98a0ad25be7857710a05c21
Abhishek Yadav <41482390+Abhi181818@users.noreply.github.com>
2023-12-13 21:34:51 +0530

    Update ThemeContext.js

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -13,27 +13,17 @@ const ThemeProvider = ({ children }) => {
                 .then((response) => {
                     const { weather } = response.data;
                     const isBadWeather = weather.some((condition) => condition.main === 'Rain' || condition.main === 'Snow' || condition.main === 'Storm');
-                    setIsNightTheme(isBadWeather || new Date().getHours() >= 18 || new Date().getHours() < 6);
+                    // setIsNightTheme(isBadWeather || new Date().getHours() >= 18 || new Date().getHours() < 6);
+                    if (isBadWeather) {
+                        setIsNightTheme(true);
+                    }
+
                 })
                 .catch((error) => console.error(error));
         });
-    }, []);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            navigator.geolocation.getCurrentPosition((position) => {
-                const { latitude, longitude } = position.coords;
-                axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=afe8699d26e89acfd2702c91649dc12b`)
-                    .then((response) => {
-                        const { weather } = response.data;
-                        const isBadWeather = weather.some((condition) => condition.main === 'Rain' || condition.main === 'Snow' || condition.main === 'Storm');
-                        setIsNightTheme(isBadWeather || new Date().getHours() >= 18 || new Date().getHours() < 6);
-                    })
-                    .catch((error) => console.error(error));
-            });
-        }, 1000 * 60 * 30); // Update weather every 30 minutes
-
-        return () => clearInterval(intervalId);
+        if (new Date().getHours() >= 18 || new Date().getHours() < 6) {
+            setIsNightTheme(true);
+        }
     }, []);
 
     const contextValue = {
@@ -41,7 +31,9 @@ const ThemeProvider = ({ children }) => {
     };
 
     return (
-        <ThemeContext.Provider value={contextValue}>{children}</ThemeContext.Provider>
+        <>
+            <ThemeContext.Provider value={contextValue}>{children}</ThemeContext.Provider>
+        </>
     );
 };
 
