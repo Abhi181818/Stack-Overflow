beae7377e9f221b882ef1a299807a137d7649e02
Abhishek Yadav <41482390+Abhi181818@users.noreply.github.com>
2023-12-13 21:14:54 +0530

    Update AllRoutes.jsx

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
 import Home from "./Pages/Home/Home";
 import Auth from "./Pages/Auth/Auth";
 import Questions from "./Pages/Questions/Questions";
